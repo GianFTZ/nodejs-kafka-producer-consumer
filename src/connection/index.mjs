3e23bd210e8a4d818fed3080a966ca6f85945b84
GianFTZ <gianfelipetz@hotmail.com>
2023-03-23 02:06:26 -0300

    feat: improove connection

--- src/connection/index.mjs
+++ src/connection/index.mjs
@@ -1,6 +1,6 @@
 import { Kafka } from "kafkajs";
 
 export const kafka = new Kafka({
-    clientId: 'kafka-nodejs-starter',
-    brokers: ['kafka1:9092']
+    clientId: 'broker',
+    brokers: ['localhost:9092']
 })
